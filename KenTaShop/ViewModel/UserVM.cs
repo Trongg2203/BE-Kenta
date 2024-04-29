dcc8af9bb74373870c9eeb2450a9874a4118719e
Trongg2203 <138137607+Trongg2203@users.noreply.github.com>
2024-04-29 09:31:37 +0700

    login and debug token,sendEmail

--- KenTaShop/ViewModel/UserVM.cs
+++ KenTaShop/ViewModel/UserVM.cs
@@ -47,5 +47,12 @@ namespace KenTaShop.ViewModel
         public string? Username { get; set; }
         public string? Email { get; set; }
     }
+
+    public class Login
+    {
+        public string? Email { get; set; }
+
+        public string Pass { get; set; } = null!;
+    }
     
 }
